485baf3813add8c3e089d9b12de8ef945d8a47b9
mmd t <mhmdttlbzd@gmail.com>
2024-07-11 10:43:17 +0430

    .

--- ConsoleApp1/ConsoleApp1/Service.cs
+++ ConsoleApp1/ConsoleApp1/Service.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    internal class Service
+    public class Service
     {
+        public bool Login(string username, string password)
+        {
+            var user = UsersList.GetByName(username);
+            if (user != null)
+            {
+                if (user.Password == password) return true;
+            }
+            return false;
+        }
+        public bool Register(string username, string password)
+        {
+            var correntUser = UsersList.GetByName(username);
+            if (correntUser != null) return false;
+            var user = new User() { Name = username, Password = password };
+            UsersList.AddUser(user);
+            return true;
+        }
+        public List<Product> GetAllProducts()
+        {
+            return Store.Products;
+        }
+        public bool AddMoney(string username, long money)
+        {
+            var user = UsersList.GetByName(username);
+            if (user != null)
+            {
+                user.Money += money;
+                return true;
+            }
+            return false;
+        }
+        public bool Buy(int productId,string username)
+        {
+            var user = UsersList.GetByName(username);
+            if (user != null) {
+                var product = Store.Products.FirstOrDefault(x => x.Id == productId);
+                if (product != null) {
+                    if (user.Money >= product.Price)
+                    {
+                        user.Products.Add(product);
+                        user.Money -= product.Price;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
     }
 }
--- ConsoleApp1/ConsoleApp1/Store.cs
+++ ConsoleApp1/ConsoleApp1/Store.cs
@@ -8,10 +8,16 @@ namespace ConsoleApp1
 {
     public static class Store
     {
-        public static List<Product> Products { get; set; } = new List<Product>();
+        public static List<Product> Products { get;private set; } = new List<Product>();
         public static void SetDefaultProducts()
         {
-            Products.Add(new PC { Id = 0,Name="PC1",Description="new pc for you",Brand="Asus",Price= });
+            Products.Add(new PC { Id = 0,Name="PC1",Description="new pc for you",Brand="Asus",Price= 20000000});
+            Products.Add(new CPU { Id = 1, Name = "CPU1", Description = "new cpu", Brand = "intel", Price = 5000000, Core = 5 });
+            Products.Add(new CPU { Id = 2, Name = "CPU2", Description = "new cpu", Brand = "intel", Price = 5000000, Core = 5 });
+            Products.Add(new Ram { Id = 3, Name = "Ram1", Description = "new ram", MemoryByte = 8000, Price = 800000 });
+            var pc = Products.First(x => x is PC)as PC;
+            pc.InnerProducts.Add(Products.First(x => x is CPU&& x.Id==2)as InnerProduct);
+            pc.InnerProducts.Add(Products.First(y => y is Ram)as InnerProduct);
         }
     }
 }
